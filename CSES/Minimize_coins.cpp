57f08f9803b4a8fff39bf63ae742923b140600d1
Asif Iqubal <57530645+asifiqubal18@users.noreply.github.com>
2025-06-12 13:12:45 +0530

    Update Minimize_coins.cpp

--- CSES/Minimize_coins.cpp
+++ CSES/Minimize_coins.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 #define mod 1000000007
 #define int long long int
-int helper(int trgrt,,vector<int> &dp){
+int helper(int trgrt,vector<int> &dp){
 if(trgt<0) return 0;
 if(trgt == 0) return 1;
 if(dp[trgt] != -1)return dp[trgt];
 for(int i=1; i<=6; i++){
-        int pick = (helper(trgt – i. dp)) % mod;
+        int pick = (helper(trgt – i ,dp)) % mod;
         int notPick = helper(trgt, dp)% mod;
   dp[n] = pick + notPick;
 }
